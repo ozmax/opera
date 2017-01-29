7d7218f29f4f45194cffe46398f6d01ed216f5de
George A. Tsakalos <ozmax.planet@gmail.com>
2017-01-29 22:33:13 +0200

    move virtuoso configuration to settings

--- opera/settings.py
+++ opera/settings.py
@@ -88,3 +88,8 @@ BROKER_URL = 'amqp://%s:%s@localhost:5672/%s' % (
 # retry every hour for the next 3 days
 RETRY_DELAY = 3600  # in seconds
 MAX_RETRIES = 72  # 3 * 24
+
+# settings for virtuoso
+VIRTUOSO_ENDPOINT = 'http://83.212.116.88:8890/DAV/xx/demo'
+
+MY_ENDPOINT = 'http://localhost:8000/'
--- virtuoso/conf.py
+++
@@ -1,3 +0,0 @@
-VIRTUOSO_ENDPOINT = 'http://83.212.116.88:8890/DAV/xx/demo'
-
-MY_ENDPOINT = 'http://localhost:8000/'
